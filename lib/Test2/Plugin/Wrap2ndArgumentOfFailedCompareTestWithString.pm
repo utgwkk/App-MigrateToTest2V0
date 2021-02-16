8be7368ccf907c6ff19e707ea428a0747b80a23a
utgwkk <utagawakiki@gmail.com>
2021-02-16 22:54:43 +0900

    Test2::Plugin::Wrap2ndArgumentOfFailedCompareTestWithString

---
+++ lib/Test2/Plugin/Wrap2ndArgumentOfFailedCompareTestWithString.pm
@@ -0,0 +1,52 @@
+package Test2::Plugin::Wrap2ndArgumentOfFailedCompareTestWithString;
+use strict;
+use warnings;
+use App::MigrateToTest2V0::Util;
+use PPI;
+use Test2::API qw(
+    test2_add_callback_post_load
+    test2_stack
+);
+
+my $loaded = 0;
+
+sub import {
+    my ($class) = @_;
+
+    return if $loaded++;
+
+    test2_add_callback_post_load(sub {
+        my $hub = test2_stack()->top;
+
+        $hub->listen(\&listener, inherit => 1);
+    });
+}
+
+sub listener {
+    my ($hub, $event) = @_;
+
+    return unless $event->causes_fail;
+
+    my $trace = $event->trace;
+    my $file = $trace->file;
+    my $line = $trace->line;
+
+    my $doc = PPI::Document->new($file);
+    my $stmt = $doc->find_first(sub {
+        my (undef, $elem) = @_;
+        return $elem->isa('PPI::Statement') && $elem->line_number == $line;
+    });
+    return unless $stmt;
+
+    my $second_arg = App::MigrateToTest2V0::Util::get_argument_of_stmt($stmt, 1);
+    return unless $second_arg;
+
+    # A -> string(A)
+    $second_arg->insert_before(PPI::Token::Word->new('string'));
+    $second_arg->insert_before(PPI::Token::Structure->new('('));
+    $second_arg->insert_after(PPI::Token::Structure->new(')'));
+
+    $doc->save($file);
+}
+
+1;
