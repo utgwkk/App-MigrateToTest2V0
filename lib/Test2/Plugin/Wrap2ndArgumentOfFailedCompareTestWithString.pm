18d627b92dd546305eec90f2d51e75e080c8f298
utgwkk <utagawakiki@gmail.com>
2021-04-18 22:02:54 +0900

    pod of Test2 plugin

--- lib/Test2/Plugin/Wrap2ndArgumentOfFailedCompareTestWithString.pm
+++ lib/Test2/Plugin/Wrap2ndArgumentOfFailedCompareTestWithString.pm
@@ -50,3 +50,46 @@ sub listener {
 }
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test2::Plugin::Wrap2ndArgumentOfFailedCompareTestWithString - A Test2 plugin that wraps 2nd argument of failed assertions with C<string()>
+
+=head1 SYNOPSIS
+
+    use Test2::Plugin::Wrap2ndArgumentOfFailedCompareTestWithString;
+
+=head1 DESCRIPTION
+
+Test2::Plugin::Wrap2ndArgumentOfFailedCompareTestWithString is a Test2 plugin that wraps 2nd argument of failed assertions with C<string()>.
+
+    use URI;
+    my $url = 'https://www.example.com'
+    my $uri = URI->new('https://www.example.com/');
+    is $url, $uri;
+
+The above test passes on Test::More because C<Test::More::is> compares both arguments as string.
+On the contrary, the test fails on Test2::V0 because C<Test2::V0::is> checks both arguments are equal structurely (a string is not a URI instance).
+
+    use URI;
+    my $url = 'https://www.example.com'
+    my $uri = URI->new('https://www.example.com/');
+    is $url, string($uri);
+
+This test passes on Test2::V0 because C<string()> enforces string comparison.
+
+=head1 LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+utgwkk E<lt>utagawakiki@gmail.comE<gt>
+
+=cut
